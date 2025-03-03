1036c1a2f53472f426594fef758031c399b2d21b
ikial-ux <kialilyas@gmail.com>
2025-03-03 20:15:48 +0100

    Remove Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import TrafficLight from './TrafficLight'; // Adjust path if necessary
 import './App.css';  // Make sure the path is correct
-import { Analytics } from '@vercel/analytics/next';
 
 function App() {
   return (
     <div className="full-screen-center">
         <TrafficLight />  {/* Include the TrafficLight component here */}
-        <Analytics /> {/* Agrega el componente de Analytics de Vercel */}
     </div>
   );
 }
