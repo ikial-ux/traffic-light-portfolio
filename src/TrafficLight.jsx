f83bcc9145779e1b0a01cb934104e694f9f9dda4
ikial-ux <kialilyas@gmail.com>
2025-03-05 15:06:32 +0100

    Improved traffic light design

--- src/TrafficLight.jsx
+++ src/TrafficLight.jsx
@@ -3,31 +3,37 @@ import './TrafficLight.css';  // Import the CSS file
 
 function TrafficLight() {
   return (
-    <div className="traffic-light">
-      <a 
-        href="https://www.linkedin.com/in/ilyas-kial-developer" 
-        target="_blank" 
-        rel="noopener noreferrer"
-        className="light red"
-      >
-        <i className="fab fa-linkedin-in"></i> {/* LinkedIn icon */}
-      </a>
-      <a 
-        href="https://github.com/ikial-ux" 
-        target="_blank" 
-        rel="noopener noreferrer"
-        className="light yellow"
-      >
-        <i className="fab fa-github"></i> {/* GitHub icon */}
-      </a>
-      <a 
-        href="/path-to-your-cv.pdf"  // Replace with your actual CV link
-        target="_blank" 
-        rel="noopener noreferrer"
-        className="light green"
-      >
-        <i className="fas fa-file-alt"></i> {/* CV icon */}
-      </a>
+    <div className="traffic-light-wrapper"> {/* Wrapper to center everything */}
+      <div className="traffic-light">
+        <div className="protector"></div>
+        <div className="protector"></div>
+        <div className="protector"></div>
+        
+        <a 
+          href="https://www.linkedin.com/in/ilyas-kial-developer" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="light red"
+        >
+          <i className="fab fa-linkedin-in"></i> {/* LinkedIn icon */}
+        </a>
+        <a 
+          href="https://github.com/ikial-ux" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="light yellow"
+        >
+          <i className="fab fa-github"></i> {/* GitHub icon */}
+        </a>
+        <a 
+          href="/path-to-your-cv.pdf"  // Replace with your actual CV link
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="light green"
+        >
+          <i className="fas fa-file-alt"></i> {/* CV icon */}
+        </a>
+      </div>
     </div>
   );
 }
