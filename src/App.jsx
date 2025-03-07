28254744834bf5a9deca8270a346e70d4a8267c0
ikial-ux <kialilyas@gmail.com>
2025-03-07 20:59:08 +0100

    Updated Vite version and fixed dev script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import TrafficLight from './TrafficLight'; // Adjust path if necessary
 import './App.css';  // Make sure the path is correct
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Traffic Light Portfolio";
+  }, []);
   return (
     <div className="full-screen-center">
         <TrafficLight />  {/* Include the TrafficLight component here */}
